a1f2cb64769abd20f95be2fad55f9b35c3930e51
Tanmay-92 <tanmaykumallick@gmail.com>
2021-08-04 09:07:04 +0530

    Welcome message for the Hotel Reservation

---
+++ HotelReservationMain.java
@@ -0,0 +1,7 @@
+package HotelReservation;
+
+public class HotelReservationMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
